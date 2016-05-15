a2837769ce9ec947eccbb9310f93954b98d091db
Damien Maillard <dmaillard06@gmail.com>
2016-05-15 20:14:30 +0200

    jsenv update

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-import transport from './lib/transporter-#{platform-type}.js';
+import transport from './lib/transporter-#{jsenv|default.agent.type}.js';
 import rest from './node_modules/@dmail/rest/index.js';
 
 var HttpService = rest.createService({
-	name: 'service-http',
+    name: 'service-http',
 
-	match(request){
-		return request.url.protocol === 'http:' || request.url.protocol === 'https:';
-	},
+    match(request) {
+        return request.url.protocol === 'http:' || request.url.protocol === 'https:';
+    },
 
-	transport(request){
-		var promise = transport(request);
+    transport(request) {
+        var promise = transport(request);
 
-		// how to abort response generation ? 
-		promise.onabort = function(){
+        // how to abort response generation ?
+        promise.onabort = function() {
 
-		};
+        };
 
-		return promise;
-	},
+        return promise;
+    },
 
-	methods: {
-		'*'(request){
-			return this.transport(request);
-		}
-	}
+    methods: {
+        '*'(request) {
+            return this.transport(request);
+        }
+    }
 });
 
 export default HttpService;
